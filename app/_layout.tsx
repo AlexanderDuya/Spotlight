5ad2d30c88e8138bb8f120acf43cf6c34c2478c6
AlexanderDuya <alexduyaconnect@gmail.com>
2025-04-06 20:37:52 +0100

    setup login using clerk, need to implement initial layout for session check for login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,23 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Missing Publishable Key");
+}
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-        <Stack screenOptions={{ headerShown: false }}></Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+            <Stack screenOptions={{ headerShown: false }}></Stack>
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
