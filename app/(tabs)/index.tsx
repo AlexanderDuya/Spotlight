07747f15acc3515f6f071a341540971e8af1bd50
AlexanderDuya <alexduyaconnect@gmail.com>
2025-04-07 16:17:09 +0100

    implemented webhook from clerk to work with convex database. users now stored in database

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../../styles/auth.styles";
 import { Link } from "expo-router";
+import { useAuth } from "@clerk/clerk-react";
 
 export default function Index() {
+  const { signOut } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Link href="/notifications">Visit Notifications Screen</Link>
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text style={{ color: "white" }}>Signout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
